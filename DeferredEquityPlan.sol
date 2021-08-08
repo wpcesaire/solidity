32576f9c20c17e5122a20a19bf26a23645b9ec0c
Whelan Power <ovpro56@gmail.com>
2021-08-07 20:16:07 -0400

    Update DeferredEquityPlan.sol

--- DeferredEquityPlan.sol
+++ DeferredEquityPlan.sol
@@ -7,13 +7,11 @@ contract DeferredEquityPlan {
     address payable employee; // bob
     bool active = true; // this employee is active at the start of the contract
 
-    // @TODO: Set the total shares and annual distribution
-    // Your code here!
+    // Set the total shares and annual distribution
 
-    uint start_time = now; // permanently store the time this contract was initialized
+    uint start_time = now; // permanently stored the time this contract was initialized
 
-    // @TODO: Set the `unlock_time` to be 365 days from now
-    // Your code here!
+    // Set the `unlock_time` to be 365 days from now
 
     uint public distributed_shares; // starts at 0
 
